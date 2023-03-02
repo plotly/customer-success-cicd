271079ceb43308d88cdc9167f3b9cabaa6c2190b
Taylor Hale Robert <taylor@plot.ly>
2023-03-01 22:46:06 -0500

    update application on Wed  1 Mar 22:46:06 EST 2023

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-import dash
-from dash import html, dcc
-import os
+from dash import Dash, dcc, html, Input, Output
+import dash_design_kit as ddk
+import plotly.express as px
 
-app = dash.Dash(__name__)
-server= app.server
+app = Dash(__name__)
+server = app.server  # expose server variable for Procfile
 
-app.layout = html.Div(children=[html.H1("My Dash App"),html.H3(f"last updated at{os.environ.get("DEPLOY_DATE")}")] )
+df = px.data.stocks()
 
-if __name__='__main__":
+app.layout = ddk.App([
+    ddk.Header([
+        ddk.Logo(src=app.get_asset_url('logo.png')),
+        ddk.Title('Dash Enterprise Sample Application'),
+    ]),
+    ddk.Card(f"Last deployed at Wed  1 Mar 22:46:06 EST 2023"),
+
+    ddk.Row(children=[
+        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AMZN", "FB"], title='Stock Prices'))),
+
+        ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AAPL", "MSFT"], title='Stock Prices')))
+    ])
+])
+
+
+if __name__ == '__main__':
     app.run_server(debug=True)
