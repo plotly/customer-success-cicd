a83c567aae3b2d8f6f227ecf2a4cf19a71034f56
Taylor Hale Robert <taylor@plot.ly>
2023-03-10 03:43:00 -0500

    update application on Fri 10 Mar 03:43:00 EST 2023

--- make_todays_app.sh
+++ make_todays_app.sh
@@ -4,6 +4,9 @@ python3 write_app.py
 git add .
 git commit -m "update application on $DEPLOY_DATE"
 git push github
+sleep 10m
 git push azure
+sleep 10m
 git push gitlab
+sleep 10m
 git push bitbucket
