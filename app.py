9cc384ad2862ac858ac038bd2151229bd04c5b9d
Taylor Hale Robert <taylor@plot.ly>
2023-03-24 07:00:01 -0400

    update application on Fri 24 Mar 07:00:01 EDT 2023

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.layout = ddk.App([
         ddk.Logo(src=app.get_asset_url('logo.png')),
         ddk.Title('Dash Enterprise Sample Application'),
     ]),
-    ddk.Card(f"Last deployed at Thu 23 Mar 07:00:01 EDT 2023"),
+    ddk.Card(f"Last deployed at Fri 24 Mar 07:00:01 EDT 2023"),
 
     ddk.Row(children=[
         ddk.Card(width=50, children=ddk.Graph(figure=px.line(df, x="date", y=["AMZN", "FB"], title='Stock Prices'))),
